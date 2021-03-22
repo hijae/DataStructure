ab7a08a3a94fe3a8c4753f898cff101f2a05ff24
희재 송 <hijae99@gmail.com>
2021-03-22 14:24:03 +0900

    ex2-10

---
+++ ex2-10/ex2-10.c
@@ -0,0 +1,18 @@
+﻿#include<stdio.h>
+
+int main()
+{
+	int i;
+	char *ptrArr[4] = { {"Korea"},{"Seoul"},{"Mapo"},{"15번지 2 / 3"} };
+	for (i = 0; i < 4; i++)
+	{
+		printf("%s\n", ptrArr[i]);
+	}
+	ptrArr[2] = "Joungno";
+	printf("\n\n");
+	for (i = 0; i < 4; i++)
+	{
+		printf("%s\n", ptrArr[i]);
+	}
+	return 0;
+}
