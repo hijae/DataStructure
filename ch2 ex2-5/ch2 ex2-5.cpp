cb2fe80ea808ae309e8a076cec571be18d2238c1
희재 송 <hijae99@gmail.com>
2021-03-22 13:20:30 +0900

    챕터2 예제2-5

---
+++ ch2 ex2-5/ch2 ex2-5.cpp
@@ -0,0 +1,17 @@
+﻿#include<stdio.h>
+int main()
+{
+	int i, leng = 0;
+	char str[50];
+	printf("\n문자열을 입력하세요 : ");
+	gets_s(str);
+	printf("\n입력된 문자열은 \n \"");
+	for (i = 0; str[i]; i++)
+	{
+		printf("%c", str[i]);
+		leng+=1;
+	}
+	printf("\" \n입니다.");
+	printf("\n\n입력된 문자열의 길이 = %d \n", leng);
+	return 0;
+}
