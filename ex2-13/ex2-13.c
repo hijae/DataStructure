612d496a91916a281a60084e8054c68faa12c53d
희재 송 <hijae99@gmail.com>
2021-03-26 11:33:02 +0900

    ex2-13

---
+++ ex2-13/ex2-13.c
@@ -0,0 +1,21 @@
+﻿#include<stdio.h>
+#include<string.h>
+struct employee
+{
+	char name[10];
+	int year;
+	int pay;
+};
+
+void main()
+{
+	struct employee Lee;
+	struct employee* Sptr = &Lee;
+	strcpy(Sptr->name, "이순신");
+	Sptr->year = 2015;
+	Sptr->pay = 5900;
+
+	printf("이름 : %s\n", Sptr->name);
+	printf("입사 : %d\n", Sptr->year);
+	printf("연봉 : %d\n\n", Sptr->pay);
+}
