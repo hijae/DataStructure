f2a1efb0e81c7c301ede73a77546433314b82a93
희재 송 <hijae99@gmail.com>
2021-04-10 16:09:50 +0900

    ch3-21, ex4-2

---
+++ ch3-21/ch3-21.c
@@ -0,0 +1,83 @@
+﻿#include <stdio.h>
+#define MAX(a,b) ((a>b)?a:b)
+#define MAX_DEGREE 50
+
+typedef struct
+{
+    int degree;
+    float coef[MAX_DEGREE];
+} polynomial;
+
+polynomial addPoly(polynomial, polynomial);
+polynomial multpoly(polynomial, polynomial);
+void printPoly(polynomial);
+
+int main()
+{
+    polynomial A = { 3,{ 4,3,5,0 } };
+    polynomial B = { 4,{ 3,1,0,2,1 } };
+
+    polynomial C;
+    C = multpoly(A, B);
+
+    printf("\n A(x) =");  printPoly(A);
+    printf("\n B(x) =");  printPoly(B);
+    printf("\n C(x) =");  printPoly(C);
+
+    return 0;
+}
+
+polynomial addPoly(polynomial A, polynomial B)
+{
+    polynomial C;
+    int A_index = 0, B_index = 0, C_index = 0;
+    int A_degree = A.degree, B_degree = B.degree;
+    C.degree = MAX(A.degree, B.degree);
+
+    while (A_index <= A.degree && B_index <= B.degree)
+    {
+        if (A_degree > B_degree)
+        {
+            C.coef[C_index++] = A.coef[A_index++];
+            A_degree--;
+        }
+        else if (A_degree == B_degree)
+        {
+            C.coef[C_index++] = A.coef[A_index++] + B.coef[B_index++];
+            A_degree--;
+            B_degree--;
+        }
+        else
+        {
+            C.coef[C_index++] = B.coef[B_index++];
+            B_degree--;
+        }
+    }
+    return C;
+}
+
+polynomial multpoly(polynomial a, polynomial b)
+{
+    int i, j;
+    polynomial c;
+    for (i = 0; i < a.degree + b.degree + 1; i++)
+        c.coef[i] = 0;
+    c.degree = a.degree + b.degree;
+    for (i = 0; i < a.degree + 1; i++)
+        for (j = 0; j < b.degree + 1; j++)
+            c.coef[i + j] += a.coef[i] * b.coef[j];
+    return c;
+}
+
+void printPoly(polynomial P)
+{
+    int i, degree;
+    degree = P.degree;
+
+    for (i = 0; i <= P.degree; i++)
+    {
+        printf("%3.0fx^%d", P.coef[i], degree--);
+        if (i < P.degree) printf(" +");
+    }
+    printf("\n");
+}
