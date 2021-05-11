ddd98f9ea564541fdd8faf952ee45fabd808cc4a
Hijae song <hijae99@gmail.com>
2021-05-11 22:54:52 +0900

    ex6-2

---
+++ bugfixex/bugfixex.cpp
@@ -0,0 +1,21 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+
+char* mm();
+
+int main()
+{
+	char* p;
+	p = mm();
+	strcpy(p, "Hello");
+	printf("%s\n", p);
+}
+
+char* mm()
+{
+	char* p;
+	p = (char*)malloc(sizeof(char) * 10);
+	return p;
+}
